240015b8ee101cb5adedcb9ab24151db6374bbed
Biloba Ginkgo <haneul20020507@gmail.com>
2025-11-28 03:59:16 +0900

    optimised

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: 'https://hnl-pk.github.io/salt_and_pepper/',
+    base: '/salt_and_pepper/',
     server: {
         open: true,
         host: true
